7ec427d82530078a6226667c340d4bbcdedc1fba
Fede <fambroggio.polko@gmail.com>
2022-12-12 18:38:31 -0300

    API RESTful

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const fs = require("fs");
-const MIN_ID = 1
-const MAX_ID = 3
 
-// MANEJO DE ARCHIVOS
+const app = express();
+const router = express.Router();
+
+// MIDDLEWARE 
 class Contenedor {
     constructor(_nombre) {
         this.nombre = _nombre;
@@ -33,7 +34,7 @@ class Contenedor {
             }
 
             const newContent = [...existingContentParsed, {
-                id: newItemId,
+                id: parseInt(newItemId),
                 ..._content,
             }]
     
@@ -64,7 +65,7 @@ class Contenedor {
             return "El archivo no existe"
         else {
             const existingContentParsed = JSON.parse(existingContent)
-            const found = existingContentParsed.find(el => el.id === _id);
+            const found = existingContentParsed.find(el => el.id === parseInt(_id));
             if (found) {
                 console.log(`Item id: ${_id}:`, found)
                 return found
@@ -86,6 +87,38 @@ class Contenedor {
         }
     }
 
+    async update(_id, _content) {
+        const existingContent = await this.read();
+        if (!existingContent) return "El archivo no existe";
+        else {
+            const existingContentParsed = JSON.parse(existingContent);
+            let found = existingContentParsed.find((el) => el.id === parseInt(_id));
+            if (found) {
+
+                const newContent = {
+                    id: paserInt(_id),
+                    ..._content,
+                }
+
+                const index = existingContentParsed.indexOf(found);
+                existingContentParsed[index] = newContent;
+
+                try {
+                    await fs.promises.writeFile(
+                        `./${this.nombre}.txt`,
+                        JSON.stringify(existingContentParsed)
+                    );
+                } catch (err) {
+                    console.log(err);
+                }
+                return paserInt(_id)
+            } else {
+                console.log(`Item id: ${_id} no fue encontrado`);
+                return null;
+            }
+        }
+    }
+
     async deleteById(_id) {
         const existingContent = await this.read()
         if (!existingContent)
@@ -93,7 +126,7 @@ class Contenedor {
         else {
             //CREAR NUEVO ARRAY, SIN EL ID SELECCIONADO 
             const existingContentParsed = JSON.parse(existingContent)
-            const result = existingContentParsed.filter(el => el.id !== _id);
+            const result = existingContentParsed.filter(el => el.id !== parseInt(_id));
 
             if (result) {       
                 try {
@@ -101,9 +134,11 @@ class Contenedor {
                 } catch (err) {
                     console.log(err);
                 }
+                return paserInt(_id)
             }
             else {
                 console.log(`Item id: ${_id} no fue encontrado`)
+                return null
             }
         }
     }
@@ -113,29 +148,57 @@ class Contenedor {
     }
 }
 
-// INICIAR SERVIDOR
-const app = express();
+const productos = new Contenedor("productos");
+
+// ENDPOINTS
+
+router.route('/')
+    .get(async (req, res) => {
+        const productosGet = await productos.getAll();
+        res.json(productosGet)
+    })
+    .post((req, res) => {
+        productos.save(req.body)
+        res.send({success: `Producto ${JSON.stringify(req.body)} añadido`})
+    })
+
+router.route('/:id')
+    .get(async (req, res) => {
+        const productoGet = await productos.getById(req.params.id)
+        if (productoGet)
+            res.send({
+                success: `Producto ${req.params.id} encontrado`,
+                data: productoGet
+            })
+        else 
+            res.send({error: "Producto no encontrado"})
+    })
+    .put(async (req, res) => {
+        const productoUpdated = productos.update(req.params.id, req.body)
+        if (productoUpdated)
+            res.send({success: `Producto ${productoUpdated} editado`})
+        else 
+            res.send({error: "Producto no encontrado"})
+    })
+    .delete((req, res) => {
+        const productoDeleted = productos.deleteById(req.params.id)
+        if (productoDeleted)
+            res.send({success: `Producto ${productoDeleted} eliminado`})
+        else 
+            res.send({error: "Producto no encontrado"})
+    })
+
+
+app.use(express.json());  // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+app.use(express.static('public')) // to serve static files
+
+app.use('/api/productos', router)
+
+// SERVER INIT
+
 const server = app.listen(8080, () => {
     console.log(`Servidor iniciado en puerto ${server.address().port}`);
 })
 // MANEJO DE ERRORES SERVIDOR
 server.on("error", (e) => console.log(e))
-
-// AUX
-const productos = new Contenedor("productos");
-
-// ENDPOINTS
-
-app.get("/", (req, res) => {
-    res.send({mensaje: " Hola mundo"})
-})
-
-app.get("/productos", async (req, res) => {
-    const productosGet = await productos.getAll();
-    res.send(productosGet)
-})
-
-app.get("/productoRandom", async (req, res) => {
-    const productoGet = await productos.getById(Math.floor(Math.random() * (MAX_ID - MIN_ID + 1) + MIN_ID))
-    res.send(productoGet)
-})
